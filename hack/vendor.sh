774286a53d732335abe001293300174cc731a951
moby <moby@example.com>
2020-07-15 13:45:41 +0200

    hack/vendor: add check for vendored archive/tar
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- hack/vendor.sh
+++ hack/vendor.sh
@@ -12,4 +12,15 @@ if ! hash vndr; then
 	exit 1
 fi
 
+if [ $# -eq 0 ]; then
+	# update our vendored copy of archive/tar when doing a full vendor
+	GO_VERSION="${GO_VERSION:=$(grep "ARG GO_VERSION" ./Dockerfile | awk -F'=' '{print $2}')}"
+	rm -rf vendor/archive
+	mkdir -p ./vendor/archive
+	git clone -b go"${GO_VERSION}" --depth=1 git://github.com/golang/go.git ./go
+	git -c user.email="moby@example.com" -c user.name="moby" --git-dir ./go/.git --work-tree ./go am ../patches/0001-archive-tar-do-not-populate-user-group-names.patch
+	cp -a go/src/archive/tar ./vendor/archive/tar
+	rm -rf ./go
+fi
+
 vndr -whitelist=^archive/tar "$@"
